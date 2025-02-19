24b92666dae5a667d375b0438866f870811f9046
garrettkatz <garrett.katz@gmail.com>
2025-02-19 12:40:12 -0500

    timing experiment

---
+++ krop.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def matrix(K):
+    H = np.array([[1]])
+    thetas = np.linspace(0, 2*np.pi, K+2)[1:-1]
+    for theta in thetas:
+        c, s = np.cos(theta), np.sin(theta)
+        H = np.block([[c*H, s*H], [s*H, -c*H]])
+
+    assert np.allclose(H.T @ H, np.eye(len(H)))
+    return H, thetas
+
+def reconstruct(thetas, idx):
+    v = np.array([1.])
+    for k, t in enumerate(thetas):
+        kth_bit = (idx >> k) & 1
+        if kth_bit == 0:
+            v = np.concatenate([np.cos(t) * v,  np.sin(t) * v])
+        else:
+            v = np.concatenate([np.sin(t) * v, -np.cos(t) * v])
+    return v
+
+def cleanup(thetas, u):
+
+    # decompose into sub-problems
+    X = u.copy()
+    for d, theta in enumerate(reversed(thetas)):
+        c, s = np.cos(theta), np.sin(theta)
+        X = X.reshape((2**(d+1), -1))
+        x_top, x_bot = X[::2], X[1::2]
+        X[::2], X[1::2] = c*x_top + s*x_bot, s*x_top - c*x_bot
+
+    # reconstruct H_i*
+    idx = np.argmax(X.flat)
+    return reconstruct(thetas, idx)
+
+
---
+++ krop_timing.py
@@ -0,0 +1,73 @@
+# big test for correctness and timing
+from time import perf_counter
+import pickle as pk
+import numpy as np
+import matplotlib.pyplot as pt
+from matplotlib import rcParams
+import krop
+
+# config for big check
+do_check = False
+num_reps = 30
+K_max = 15
+
+if do_check:
+
+    runtimes = {"direct": {}, "krop": {}}
+
+    for K in range(1, K_max+1):
+        N = 2**K
+
+        # build krop matrix
+        print(f"{K=} build...")
+        H, thetas = krop.matrix(K)
+
+        # time the cleanup
+        print(f"{K=} timing...")
+        runtimes["direct"][K] = []
+        runtimes["krop"][K] = []
+
+        for rep in range(num_reps):
+            u = np.random.randn(N)
+
+            start = perf_counter()
+            v_direct = H[(H.T @ u).argmax()]
+            duration = perf_counter() - start
+            runtimes["direct"][K].append(duration)
+
+            start = perf_counter()
+            v_krop = krop.cleanup(thetas, u)
+            duration = perf_counter() - start
+            runtimes["krop"][K].append(duration)
+            
+            assert np.allclose(v_direct, v_krop)
+
+    with open("krop_timing.pkl","wb") as f: pk.dump(runtimes, f)
+
+with open("krop_timing.pkl","rb") as f: runtimes = pk.load(f)
+
+rcParams["font.family"] = "serif"
+rcParams["font.size"] = 12
+rcParams["text.usetex"] = True
+
+pt.figure(figsize=(8,2.5))
+markers = {"krop": "x", "direct": "o"}
+means = {"krop": [], "direct": []}
+for key, data in runtimes.items():
+    for i, (K, results) in enumerate(data.items()):
+        pt.plot(K + 0.05*np.random.randn(len(results)), results, markers[key], mec=(.75,)*3, mfc="none", linestyle="none")
+        means[key].append(np.mean(results))
+
+for key, data in means.items():
+    pt.plot(1+np.arange(K_max), data, markers[key] + "k-", mfc="none", label=key)
+
+pt.legend()
+pt.xlabel("$K = \\mathrm{log}_2 N$")
+pt.ylabel("Runtime (seconds)")
+pt.yscale("log")
+pt.tight_layout()
+pt.savefig("krop_timing.eps")
+pt.show()
+
+
+
